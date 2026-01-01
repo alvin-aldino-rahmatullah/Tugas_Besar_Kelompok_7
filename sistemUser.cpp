8311153b3dc475efb839012a7fd7b8f33b3b9524
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-01 10:10:00 +0700

    Implementasi Sistem User

---
+++ sistemUser.cpp
@@ -0,0 +1,16 @@
+#include "sistem.h"
+#include "iostream"
+
+using namespace std;
+
+void lokasiPeminjaman(){
+   cout << "Lokasi Peminjaman dipilih"<< endl;
+}
+
+void alatTambahan(){
+   cout << "Alat Tambahan dipilih"<< endl;
+}
+
+void jadwalPemakaian(){
+   cout << "Jadwal Pemakaian dipilih"<< endl;
+}
