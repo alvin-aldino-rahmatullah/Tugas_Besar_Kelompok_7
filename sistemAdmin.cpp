aafc58a84fa9f0a0a38d15b67e68302a11692f22
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-01 10:08:03 +0700

    Implementasi Sistem Admin

---
+++ sistemAdmin.cpp
@@ -0,0 +1,12 @@
+#include "sistem.h"
+#include "iostream"
+
+using namespace std;
+
+void daftarPeminjam(){
+   cout << "Daftar Peminjam dipilih"<< endl;
+}
+
+void viewPeminjam(){
+   cout << "View Peminjam dipilih"<< endl;
+}
