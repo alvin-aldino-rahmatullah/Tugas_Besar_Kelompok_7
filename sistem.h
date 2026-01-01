795193aaae73ca93e62f64d130ded7fd005f299f
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-01 10:03:53 +0700

    Update Void Sistem

---
+++ sistem.h
@@ -0,0 +1,17 @@
+#ifndef SISTEM_H
+#define SISTEM_H
+#include "string"
+
+std::string login();
+void adminMenu();
+void userMenu();
+void countdown(int seconds);
+
+void daftarPeminjam();
+void viewPeminjam();
+
+void lokasiPeminjaman();
+void jadwalPemakaian();
+void alatTambahan();
+
+#endif
