f865d6e7a24f50c621a4d4b0cb9ebad03b527145
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-01 14:46:18 +0700

    feat: implement login using CSV user database

--- login.cpp
+++ login.cpp
@@ -3,7 +3,58 @@
 #include "string"
 #include "thread"
 #include "chrono"
+#include "fstream"
+#include "sstream"
+
 using namespace std;
+
+bool authenticateUser(
+    const string& inputUsername,
+    const string& inputPassword,
+    User& currentUser
+) {
+    ifstream file("database/Users.csv");
+    if (!file.is_open()) {
+        cout << "Gagal membuka database user.\n";
+        return false;
+    }
+
+    string line;
+    getline(file, line);
+
+    while (getline(file, line)) {
+        stringstream stream(line);
+        string temp;
+
+        User user;
+
+        getline(stream, temp, ';');
+        user.id = stoi(temp);
+
+        getline(stream, user.username, ';');
+        getline(stream, user.password, ';');
+        getline(stream, user.role, ';');
+        getline(stream, user.status, ';');
+
+        user.username.erase(
+            user.username.find_last_not_of(" \n\r\t") + 1
+        );
+
+        if (user.username == inputUsername &&
+            user.password == inputPassword) {
+
+            if (user.status != "active") {
+                cout << "Akun belum aktif, hubungi admin.\n";
+                return false;
+            }
+
+            currentUser = user;
+            return true;
+        }
+    }
+    return false;
+}
+
 
 void countdown(int detik) {
     cout << endl; 
@@ -15,7 +66,9 @@ void countdown(int detik) {
     cout << endl; 
 }
 
-string login() {
+
+void login() {
+    User currentUser;
     string username, password;
     int percobaan = 0;
     const int maksimal = 3;
@@ -29,15 +82,13 @@ string login() {
         cout << "Password: ";
         cin >> password;
 
-        if (username == "admin" && password == "admin123") {
-            cout << "-[Login sebagai Admin berhasil.]-" << endl;
-            cout << endl; 
-            return "admin";
-        }
-        else if (username == "user" && password == "user123") {
-            cout << "-[Login sebagai User berhasil.]-" << endl;
-            cout << endl; 
-            return "user";
+        if (authenticateUser(username, password, currentUser)) {
+            cout << "Login berhasil sebagai " << currentUser.role << endl;
+            if (currentUser.role == "admin") {
+                adminMenu();
+            } else if (currentUser.role == "user") {
+                userMenu();
+            }
         }
         else {
             percobaan++;
