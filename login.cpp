677dce82eadcfb1d7f2d2e3121b4956959acc36f
alvin-aldino-rahmatullah <alvin.aldino.rahmatullahh@gmail.com>
2026-01-01 11:09:00 +0700

    Add login cooldown after failed attempts

--- login.cpp
+++ login.cpp
@@ -1,29 +1,57 @@
 #include "sistem.h"
-#include <iostream>
-#include <string>
-
+#include "iostream"
+#include "string"
+#include "thread"
+#include "chrono"
 using namespace std;
+
+void countdown(int detik) {
+    cout << endl; 
+    for (int i = detik; i > 0; --i)
+    {
+        cout << "Silahkan Tunggu Selama " << i << " Detik..." << endl;
+        this_thread::sleep_for(chrono::seconds(1));
+    }
+    cout << endl; 
+}
 
 string login() {
     string username, password;
+    int percobaan = 0;
+    const int maksimal = 3;
+    const int berhenti = 10;
 
-    cout << "Username: ";
-    cin >> username;
+    while (true) {
+        cout << endl; 
+        cout << "Username: ";
+        cin >> username;
 
-    cout << "Password: ";
-    cin >> password;
+        cout << "Password: ";
+        cin >> password;
 
-    if (username == "admin" && password == "admin123") {
-        cout << "Login sebagai Admin berhasil." << endl;
-        return "admin";
-    }
-    else if (username == "user" && password == "user123") {
-        cout << "Login sebagai User berhasil." << endl;
-        return "user";
-    }
-    else {
-        cout << "Login gagal." << endl;
-        return "gagal";
+        if (username == "admin" && password == "admin123") {
+            cout << "-[Login sebagai Admin berhasil.]-" << endl;
+            cout << endl; 
+            return "admin";
+        }
+        else if (username == "user" && password == "user123") {
+            cout << "-[Login sebagai User berhasil.]-" << endl;
+            cout << endl; 
+            return "user";
+        }
+        else {
+            percobaan++;
+            cout << "-[login gagal! percobaan ke " << percobaan << "]-" << endl;
+
+            if (percobaan == maksimal) {
+                cout << endl; 
+                cout << "--TERLALU BANYAK PERCOBAAN GAGAL--" << endl;
+                cout << "--AKSES LOGIN DIBLOKIR SEMENTARA--" << endl;
+
+                countdown(berhenti);
+                percobaan = 0;
+                cout << "--SILAHKAN MENCOBA LOGIN KEMBALI--" << endl;
+            }
+        }
     }
 }
-
